42b47d8efd9dc0af4338d2e3c25759808acc64b0
ozturkeniss <enisozturk75@gmail.com>
2025-10-22 02:51:12 +0300

    Remove unused ErrorResponse struct from ProductsResponse model to streamline code and improve maintainability.

--- internal/product/model.go
+++ internal/product/model.go
@@ -52,11 +52,6 @@ type ProductsResponse struct {
 	Count    int       `json:"count"`
 }
 
-// ErrorResponse represents an error response
-type ErrorResponse struct {
-	Error   string `json:"error"`
-	Message string `json:"message,omitempty"`
-}
 
 // SuccessResponse represents a success response
 type SuccessResponse struct {
