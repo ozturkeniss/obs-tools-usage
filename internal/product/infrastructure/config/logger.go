fba5af3d381e3e279c302ac775edd63f77f29d34
ozturkeniss <enisozturk75@gmail.com>
2025-10-23 22:50:28 +0300

    Remove unused getEnv function from logger.go to streamline code and enhance clarity

--- internal/product/infrastructure/config/logger.go
+++ internal/product/infrastructure/config/logger.go
@@ -177,11 +177,4 @@ func GetLogger() *logrus.Logger {
 	return Logger
 }
 
-// getEnv gets an environment variable with a default value
-func getEnv(key, defaultValue string) string {
-	if value := os.Getenv(key); value != "" {
-		return value
-	}
-	return defaultValue
-}
 
